f3d6eb29a9f1c1ebe78d1a1f27598e7bbb19a04d
Andres Molinares <andresmolinaresb@gmail.com>
2021-04-12 12:01:57 -0500

    Login con validacion terminada

--- modelo/validar_login.php
+++ modelo/validar_login.php
@@ -1,3 +1,25 @@
 <?php
-    $nit=
-?>
+    $nit=$_POST['nit'];
+    $contrasena=$_POST['contraseña'];
+    session_start();
+    $_SESSION['nit']=$nit;
+
+    include('../conexion/conexion.php');
+
+    $consulta="SELECT * FROM usuario WHERE nit='$nit' AND contrasena='$contrasena'";
+    $resultado=mysqli_query($conexion, $consulta);
+
+    $filas = mysqli_num_rows($resultado);
+
+    if($filas){
+        header("location:../vista/index.html");
+    }else{
+        ?>
+        <?php
+        include("../vista/index.html");
+        ?>
+        <h1 class="bad">Error en la autenticación</h1>
+        <?php
+    }
+mysqli_free_result($resultado);
+mysqli_close($conexion);
