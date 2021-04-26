778b76989a31eee677521cca8b24f2e04d5c35ef
Andres Molinares <andresmolinaresb@gmail.com>
2021-04-26 13:36:01 -0500

    Vista por estandares: Se empezo con la creacion de las vistas por estandares

---
+++ vista/estandares/estandar-1.php
@@ -0,0 +1,169 @@
+<?php
+session_start();
+$nit = $_SESSION['nit'];
+
+if (!isset($nit)) {
+    header("location:../vista/login.html");
+} else {
+
+?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Estandar 1</title>
+        <link rel="stylesheet" href="../../estilos/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../../estilos/sidebar-style.css">
+
+    </head>
+
+    <!--NAVBAR VERTICAL-->
+    <div class="vertical-nav bg-dark" id="sidebar">
+        <div class="py-4 px-3 mb-4 bg-dark">
+            <div class="media d-flex align-items-center">
+                <img src="../../img/logo-unisinu-cartagena-rojo.png" alt="..." width="200" class="mr-3 shadow-sm bg-dark">
+
+            </div>
+        </div>
+
+        <p class="text-gray font-weight-bold text-uppercase px-3 small pb-4 mb-0">Principal</p>
+
+        <ul class="nav flex-column bg-dark mb-0">
+            <li class="nav-item">
+                <a href="../dashboard.php" class="nav-link text-light font-italic">
+                    <i class="fa fa-th-large mr-3 text-primary fa-fw"></i>
+                    Inicio
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-light font-italic">
+                    <i class="fa fa-address-card mr-3 text-primary fa-fw"></i>
+                    About
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-light font-italic">
+                    <i class="fa fa-cubes mr-3 text-primary fa-fw"></i>
+                    Services
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-light font-italic">
+                    <i class="fa fa-picture-o mr-3 text-primary fa-fw"></i>
+                    Gallery
+                </a>
+            </li>
+        </ul>
+
+        <p class="text-gray font-weight-bold text-uppercase px-3 small py-4 mb-0">Sistemas de gestión</p>
+
+        <ul class="nav flex-column bg-dark mb-0">
+            <li class="nav-item">
+                <a href="#" class="nav-link text-light font-italic  bg-gradient">
+                    <i class="fa fa-area-chart mr-3 text-primary fa-fw"></i>
+                    Seguridad y salud del trabajo
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-light font-italic">
+                    <i class="fa fa-bar-chart mr-3 text-primary fa-fw"></i>
+                    Gestión ambiental
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-light font-italic">
+                    <i class="fa fa-pie-chart mr-3 text-primary fa-fw"></i>
+                    Gestión de calidad
+                </a>
+            </li>
+
+        </ul>
+
+        <p class="text-gray font-weight-bold text-uppercase px-3 small py-2 mb-0">Más</p>
+        <ul class="nav flex-column bg-dark mb-0">
+            <li class="nav-item">
+                <a href="../modelo/cerrar_sesion.php" class="nav-link text-light font-italic">
+                    <button class="btn btn-danger">Cerrar sesión</button></br></br>
+                </a>
+
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-light font-italic">Todos los derechos reservados.</a>
+            </li>
+        </ul>
+
+    </div>
+    <!-- End vertical navbar -->
+
+
+    <body>
+        <div class="page-content p-5  p-3 mb-5 bg-body rounded" id="content">
+            <h3 class="display-4 text-dark fw-bold">Estandar 1</h3>
+
+            <table class="table table-striped table-hover">
+                <thead class="bg-dark text-light">
+                    <tr>
+                        <th>Item</th>
+                        <th>Nombre</th>
+                        <th>Progreso</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <!--Inicio de item-->
+                    <tr>
+                        <td>1.1</td>
+                        <td>Recuros Financieros Tecnicos y Humanos</td>
+                        <td>
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
+                            </div>
+                        </td>
+                        <td>
+                            <a href="#" class="btn btn-warning">Ver</a>
+                            |
+                            <a href="#" class="btn btn-primary">Descargar</a>
+                        </td>
+                    </tr>
+                    <!--Fin de Item-->
+
+                        <!--Inicio de item-->
+                        <tr>
+                        <td>1.2</td>
+                        <td>Capacitacion del Sistema SST</td>
+                        <td>
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" style="width: 50%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">50%</div>
+                            </div>
+                        </td>
+                        <td>
+                            <a href="#" class="btn btn-warning">Ver</a>
+                            |
+                            <a href="#" class="btn btn-primary">Descargar</a>
+                        </td>
+                    </tr>
+                    <!--Fin de Item-->
+
+                </tbody>
+
+            </table>
+
+
+
+
+
+        </div>
+
+
+
+    </body>
+
+    </html>
+
+<?php
+}
+?>
