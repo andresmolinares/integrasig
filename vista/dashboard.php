e95bc8312cb68874c6903213b7729b46d48114e0
Andres Molinares <andresmolinaresb@gmail.com>
2021-05-06 23:24:37 -0500

    Se agrego carrusel de fotos al dashboard

--- vista/dashboard.php
+++ vista/dashboard.php
@@ -127,6 +127,31 @@ if (!isset($nit)) {
 
           <div class="separator"></div>
 
+          <!--Carrusel de fotos-->
+          <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+
+              <div class="carousel-item active">
+                <img src="../img/visionIntegrasig.png" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="../img/FilosofiaIntegrasig.png" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="../img/imagen.png" class="d-block w-100" alt="...">
+              </div>
+            </div>
+
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+
         </div>
 
 
